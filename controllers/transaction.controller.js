ca8e7b99ebc9e119207c81f750f1e1851998976d
David Atanda <ololadedavid15@gmail.com>
2023-08-13 14:27:16 +0100

    done with transactions

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -7,5 +7,15 @@ const getTransactions = (req, res)=>{
         res.status(500).json({message: 'Internal Server Error'})
     })
 }
+const addTransaction = (req, res)=>{
+    const payload = req.body
+    payload.created_at = new Date().toISOString()
+    let form = new TransactionModel(payload)
+    form.save().then(()=>{
+        res.status(200).json({message: 'Success'})
+    }).catch(()=>{
+        res.status(500).json({message: 'Internal Server Error'})
+    })
+}
 
-module.exports = { getTransactions }
+module.exports = { getTransactions,addTransaction }
