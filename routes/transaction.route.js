da8934dbc09fb79088249f76273848a73de634bf
David Atanda <ololadedavid15@gmail.com>
2023-08-14 16:41:57 +0100

    consruct a patch route to delete transaction

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { getTransactions, addTransaction } = require('../controllers/transaction.controller')
+const { getTransactions, addTransaction, deleteTransaction } = require('../controllers/transaction.controller')
 const TransactionRouter = express.Router()
 
 TransactionRouter.get('/all', getTransactions)
 TransactionRouter.post('/add', addTransaction)
+TransactionRouter.patch('/delete', deleteTransaction)
 
 module.exports = TransactionRouter
