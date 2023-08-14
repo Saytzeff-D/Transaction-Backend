d5655e0fb9056ab899eb5b7d8642afaa56915047
David Atanda <ololadedavid15@gmail.com>
2023-08-14 16:40:50 +0100

    construct the user route

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ require('dotenv').config()
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const TransactionRouter = require('./routes/transaction.route')
+const UserRouter = require('./routes/user.route')
 app.use(cors({origin: '*'}))
 app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}))
 app.use(bodyParser.json({limit:'50mb'}));
 app.use('/transact', TransactionRouter)
+app.use('/user', UserRouter)
 
 mongoose.connect(process.env.URI).then(()=>{
     console.log('Mongo Database Connected Successfully')
