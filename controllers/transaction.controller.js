69a7a9a7c53fdac6a3ec9ed5b8510186265f4e17
David Atanda <ololadedavid15@gmail.com>
2023-08-19 18:46:50 +0100

    now fetching only today's transaction

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -2,7 +2,10 @@ const TransactionModel = require("../models/transaction.model")
 
 const getTransactions = (req, res)=>{
     TransactionModel.find().then(result=>{
-        res.status(200).json({transactions: result})
+        const today = new Date().toLocaleDateString()
+        filterResult = result.filter(each=>new Date(each.created_at).toLocaleDateString() == today)
+        console.log(filterResult)
+        res.status(200).json({transactions: filterResult})
     }).catch(err=>{
         res.status(500).json({message: 'Internal Server Error'})
     })
@@ -18,7 +21,8 @@ const addTransaction = (req, res)=>{
     })
 }
 const deleteTransaction = (req, res)=>{
-    const payload = req.body    
+    const payload = req.body  
+    console.log(payload)  
     TransactionModel.findByIdAndDelete(payload.id).then(result=>{
         console.log(result)
         res.status(200).json({message: 'Success'})
